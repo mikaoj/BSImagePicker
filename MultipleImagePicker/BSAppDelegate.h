5b8bad2ebfeca63698c903eb4638282663208a27
Joakim Gyllstrom <joakim@backslashed.se>
2014-04-05 00:01:19 +0200

    Initial Commit

---
+++ MultipleImagePicker/BSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BSAppDelegate.h
+//  MultipleImagePicker
+//
+//  Created by Joakim Gyllström on 2014-04-05.
+//  Copyright (c) 2014 Joakim Gyllström. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MultipleImagePicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MultipleImagePicker
+//
+//  Created by Joakim Gyllström on 2014-04-05.
+//  Copyright (c) 2014 Joakim Gyllström. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BSAppDelegate class]));
+    }
+}
