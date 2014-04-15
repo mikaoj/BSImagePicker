69ef7204398d63c1bbfb4574852d04cbca3a7ed3
Joakim Gyllstrom <joakim@backslashed.se>
2014-04-15 21:02:14 +0200

    Added UIImagePicker (for comparison)

--- MultipleImagePicker/Controller/BSAppDelegate.m
+++ MultipleImagePicker/Controller/BSAppDelegate.m
@@ -10,9 +10,10 @@
 #import "BSImageSelectionController.h"
 #import "BSImagePickerController.h"
 
-@interface BSAppDelegate ()
+@interface BSAppDelegate () <UIImagePickerControllerDelegate>
 
 @property (nonatomic, strong) BSImagePickerController *imagePicker;
+@property (nonatomic, strong) UIImagePickerController *oldImagePicker;
 
 @end
 
@@ -25,14 +26,22 @@
     self.window.backgroundColor = [UIColor whiteColor];
     
     UIViewController *viewController = [[UIViewController alloc] init];
-    UIButton *pressMe = [[UIButton alloc] initWithFrame:CGRectMake(100, 300, 100, 35)];
-    [pressMe setTitle:@"Press me" forState:UIControlStateNormal];
+    
+    UIButton *pressMe2 = [[UIButton alloc] initWithFrame:CGRectMake(50, 200, 200, 35)];
+    [pressMe2 setTitle:@"UIImagePicker" forState:UIControlStateNormal];
+    [pressMe2 setTitleColor:viewController.view.tintColor forState:UIControlStateNormal];
+    [pressMe2 addTarget:self action:@selector(doTheUgly:) forControlEvents:UIControlEventTouchUpInside];
+    [viewController.view addSubview:pressMe2];
+    
+    UIButton *pressMe = [[UIButton alloc] initWithFrame:CGRectMake(50, 300, 200, 35)];
+    [pressMe setTitle:@"BSImagePicker" forState:UIControlStateNormal];
     [pressMe setTitleColor:viewController.view.tintColor forState:UIControlStateNormal];
     [pressMe addTarget:self action:@selector(doTheMagic:) forControlEvents:UIControlEventTouchUpInside];
     [viewController.view addSubview:pressMe];
     
     
     [self setImagePicker:[[BSImagePickerController alloc] init]];
+    [self setOldImagePicker:[[UIImagePickerController alloc] init]];
     [self.window setRootViewController:viewController];
     
     [self.window makeKeyAndVisible];
@@ -40,24 +49,97 @@
     return YES;
 }
 
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+
+
+- (void)doTheUgly:(id)sender
+{
+    [self.oldImagePicker setDelegate:self];
+    
+    [self.window.rootViewController presentViewController:self.oldImagePicker animated:YES completion:nil];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 - (void)doTheMagic:(id)sender
 {
     [self.window.rootViewController presentImagePickerController:self.imagePicker
                                                         animated:YES
                                                       completion:nil
-                                                       toggle:^(NSDictionary *info, BOOL selected) {
-                                                           if(selected) {
-                                                               NSLog(@"Do logic here for selecting image");
-                                                           } else {
-                                                               NSLog(@"Do logic here for deselecting image");
-                                                           }
-                                                      } finish:^(NSArray *infoArray, BOOL canceled) {
-                                                          if(canceled) {
-                                                              NSLog(@"Do logic here for picker cancel");
-                                                          } else {
-                                                              NSLog(@"Do logic here for picker done");
+                                                          toggle:^(NSDictionary *info, BOOL selected) {
+                                                              if(selected) {
+                                                                  NSLog(@"Do logic here for selecting image");
+                                                              } else {
+                                                                  NSLog(@"Do logic here for deselecting image");
+                                                              }
                                                           }
-                                                      }];
+                                                          finish:^(NSArray *infoArray, BOOL canceled) {
+                                                              if(canceled) {
+                                                                  NSLog(@"Do logic here for picker cancel");
+                                                              } else {
+                                                                  NSLog(@"Do logic here for picker done");
+                                                              }
+                                                          }];
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
