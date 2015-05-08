ee75b6cebb6bf3ed63df871d75422e73fcf7386b
Joakim Gyllstrom <joakim@backslashed.se>
2015-05-08 16:26:17 +0200

    Dont cache for now...i keep running in to memory warnings and crashes. Works smooth on an iphone 6 without it

--- Pod/Classes/PhotosDataSource.swift
+++ Pod/Classes/PhotosDataSource.swift
@@ -10,17 +10,29 @@ import UIKit
 import Photos
 
 internal class PhotosDataSource : NSObject, UICollectionViewDataSource {
-    internal var imageSize: CGSize? {
-        willSet { stopCaching() }
-        didSet { startCaching() }
+    internal var imageSize: CGSize = CGSize(width: 0, height: 0) {
+        willSet {
+            if CGSizeEqualToSize(newValue, imageSize) == false {
+                stopCaching()
+            }
+        }
+        didSet {
+            if CGSizeEqualToSize(oldValue, imageSize) == false {
+                startCaching()
+            }
+        }
     }
     
     private let photoCellIdentifier = "photoCellIdentifier"
     private let photosManager = PHCachingImageManager()
     private let imageContentMode: PHImageContentMode = .AspectFill
-    private var assets: [PHAsset] = [] {
-        willSet { stopCaching() }
-        didSet { startCaching() }
+    private var assets: [PHAsset]? {
+        willSet {
+            stopCaching()
+        }
+        didSet {
+            startCaching()
+        }
     }
     
     override init() {
@@ -29,11 +41,13 @@ internal class PhotosDataSource : NSObject, UICollectionViewDataSource {
             NSSortDescriptor(key: "creationDate", ascending: false)
         ]
         
-        if let results = PHAsset.fetchAssetsWithMediaType(.Image, options: fetchOptions) {
+        if let result = PHAsset.fetchAssetsWithMediaType(.Image, options: fetchOptions) {
             var assets: [PHAsset] = []
-            results.enumerateObjectsUsingBlock { (object, idx, _) in
+            result.enumerateObjectsUsingBlock { (object, idx, _) in
                 if let asset = object as? PHAsset {
                     assets.append(asset)
+                } else {
+                    println("not!")
                 }
             }
             
@@ -43,8 +57,16 @@ internal class PhotosDataSource : NSObject, UICollectionViewDataSource {
         super.init()
     }
     
+    deinit {
+//        photosManager.stopCachingImagesForAllAssets()
+    }
+    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return assets.count
+        if let assets = assets {
+            return assets.count
+        }
+        
+        return 0
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -54,23 +76,29 @@ internal class PhotosDataSource : NSObject, UICollectionViewDataSource {
             photosManager.cancelImageRequest(PHImageRequestID(cell.tag))
         }
         
-        if let imageSize = imageSize {
+        if let assets = assets {
             let asset = assets[indexPath.row]
             cell.tag = Int(photosManager.requestImageForAsset(asset, targetSize: imageSize, contentMode: imageContentMode, options: nil) { (result, _) in
                 cell.imageView.image = result
-                })
+            })
         }
         
         return cell
     }
     
     private func stopCaching() {
-        photosManager.stopCachingImagesForAllAssets()
+//        if let assets = assets {
+//            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+//                self.photosManager.stopCachingImagesForAssets(assets, targetSize: self.imageSize, contentMode: self.imageContentMode, options: nil)
+//            }
+//        }
     }
     
     private func startCaching() {
-        if let imageSize = imageSize {
-            photosManager.startCachingImagesForAssets(self.assets, targetSize: imageSize, contentMode: imageContentMode, options: nil)
-        }
+//        if let assets = assets {
+//            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+//                self.photosManager.startCachingImagesForAssets(assets, targetSize: self.imageSize, contentMode: self.imageContentMode, options: nil)
+//            }
+//        }
     }
 }
