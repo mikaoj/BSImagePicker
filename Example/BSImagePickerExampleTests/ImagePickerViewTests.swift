c6d3fa2f18374b9b3c4990bfadeadc13055991ab
Joakim Gyllstrom <joakim@backslashed.se>
2015-05-21 18:23:18 +0200

    do some view testing here later

---
+++ Example/BSImagePickerExampleTests/ImagePickerViewTests.swift
@@ -0,0 +1,23 @@
+//
+//  ImagePickerViewTests.swift
+//  BSImagePickerExample
+//
+//  Created by Joakim Gyllström on 2015-05-21.
+//  Copyright (c) 2015 Joakim Gyllström. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import FBSnapshotTestCase
+import BSImagePicker
+
+class ImagePickerViewTests: FBSnapshotTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testNothing() {
+        XCTAssert(true, "add view tests")
+    }
+}
