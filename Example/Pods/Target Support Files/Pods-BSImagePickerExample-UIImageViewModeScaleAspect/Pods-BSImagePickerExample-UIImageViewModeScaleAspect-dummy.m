02598f650263462646b0e6f6b8a8e994c944ffe4
Joakim Gyllstrom <joakim@backslashed.se>
2015-07-23 14:16:53 +0200

    Pod update with 0.38

--- Example/Pods/Target Support Files/Pods-BSImagePickerExample-UIImageViewModeScaleAspect/Pods-BSImagePickerExample-UIImageViewModeScaleAspect-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BSImagePickerExample_UIImageViewModeScaleAspect : NSObject
-@end
-@implementation PodsDummy_Pods_BSImagePickerExample_UIImageViewModeScaleAspect
-@end
