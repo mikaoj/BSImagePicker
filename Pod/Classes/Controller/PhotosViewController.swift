1c4bb26675bba94004692e2280745e095dcf1c5e
Joakim Gyllström <joakim@backslashed.se>
2018-07-31 20:30:31 +0200

    Silenced flatMap -> compactMap warning

--- Pod/Classes/Controller/PhotosViewController.swift
+++ Pod/Classes/Controller/PhotosViewController.swift
@@ -312,7 +312,7 @@ extension PhotosViewController {
             updateDoneButton()
 
             // Get indexPaths of selected items
-            let selectedIndexPaths = photosDataSource.selections.flatMap({ (asset) -> IndexPath? in
+            let selectedIndexPaths = photosDataSource.selections.compactMap({ (asset) -> IndexPath? in
                 let index = photosDataSource.fetchResult.index(of: asset)
                 guard index != NSNotFound else { return nil }
                 return IndexPath(item: index, section: 1)
