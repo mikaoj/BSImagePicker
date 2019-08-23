47a3f2c722e2fbcb95082d6a2fbd963801288553
Joakim Gyllström <joakim@backslashed.se>
2019-08-23 09:34:53 +0200

    Working UI tests

--- UITests/UITests.swift
+++ UITests/UITests.swift
@@ -24,46 +24,46 @@ import XCTest
 import Photos
 
 class UITests: XCTestCase {
-    
+
     let app = XCUIApplication()
-    
+
     override func setUp() {
         super.setUp()
-        
+
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         app.launch()
-        
+
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         app.terminate()
         super.tearDown()
     }
-    
+
     func testDoneButtonDisabledOnStartup() {
         app.buttons["Image picker"].tap()
         XCTAssert(app.navigationBars.buttons["Done"].isEnabled == false)
     }
-    
+
     func _testDoneButtonEnabledAfterSelection() {
         app.buttons["Image picker"].tap()
         app.collectionViews.children(matching: .any).element(boundBy: 0).tap()
         XCTAssert(app.navigationBars.buttons["Done (1)"].isEnabled == true)
     }
-    
+
     func _testDoneButtonDisabledAfterDeselection() {
         app.buttons["Image picker"].tap()
         app.collectionViews.children(matching: .any).element(boundBy: 0).tap()
         app.collectionViews.children(matching: .any).element(boundBy: 0).tap()
         XCTAssert(app.navigationBars.buttons["Done"].isEnabled == false)
     }
-    
+
     func _testLongpressPreview() {
         app.buttons["Image picker"].tap()
         app.collectionViews.children(matching: .any).element(boundBy: 0).press(forDuration: 1.0)
