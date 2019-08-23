bafdccf938eac5e130abb47224ba4d7d292c271a
Joakim Gyllström <joakim@backslashed.se>
2019-08-23 11:22:27 +0200

    Swift package doesn't have support for resources (images) yet. So lets leave it here and get back to it when it does

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BSImagePicker",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "Tests",
-            dependencies: ["BSImagePicker"]),
+            dependencies: ["BSImagePicker"],
+            path: "Tests"),
     ]
 )
