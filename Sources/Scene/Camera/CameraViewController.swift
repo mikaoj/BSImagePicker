2cb29ce4ebbabfde167a1ed0c98268caf067d666
Joakim Gyllström <joakim@backslashed.se>
2019-09-22 01:10:23 +0200

    WIP: CameraViewController

--- Sources/Scene/Camera/CameraViewController.swift
+++ Sources/Scene/Camera/CameraViewController.swift
@@ -21,7 +21,48 @@
 // SOFTWARE.
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
-    
+    private let captureSession = AVCaptureSession()
+    private let previewView = CameraPreviewView()
+    private let captureSessionQueue = DispatchQueue(label: "session queue")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        previewView.session = captureSession
+        previewView.frame = view.bounds
+        previewView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(previewView)
+        
+        requestAuthorization()
+        setupSession()
+    }
+
+    private func requestAuthorization() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            break
+        case .notDetermined:
+            captureSessionQueue.suspend()
+            AVCaptureDevice.requestAccess(for: .video, completionHandler: { [weak self] granted in
+                if granted {
+                    self?.captureSessionQueue.resume()
+                } else {
+                    // TODO: User didn't grant access. Show something?
+                }
+            })
+
+        default:
+            // TODO: User has denied access...show some sort of dialog..?
+            break
+        }
+    }
+
+    private func setupSession() {
+        captureSessionQueue.async {
+
+        }
+    }
 }
